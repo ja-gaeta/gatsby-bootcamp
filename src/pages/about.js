b6725fe8c759f43e08ec34f682d1d0ccd1296c39
gaeta <ja.gaeta1@gmail.com>
2020-04-01 16:08:38 -0300

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>I currently teach full-time on Udemy.</p>
+    </div>
+  )
+}
+
+export default AboutPage
