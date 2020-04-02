ed08c11bb7407d4c0d22384a717aea7557479ae9
gaeta <ja.gaeta1@gmail.com>
2020-04-02 11:43:49 -0300

    Styling with CSS Modules

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,12 +3,15 @@ import React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
 import "../styles/index.scss"
+import layoutStyles from "./layout.module.scss"
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <Header />
-      {children}
+    <div className={layoutStyles.container}>
+      <div className={layoutStyles.content}>
+        <Header />
+        {children}
+      </div>
       <Footer />
     </div>
   )
