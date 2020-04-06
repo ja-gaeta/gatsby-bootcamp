1e7376e000dbf0aa217d3a0daf7c42a0622b91a6
gaeta <ja.gaeta1@gmail.com>
2020-04-06 10:39:49 -0300

    Installed Remark and configured gatsby-node.js to generate slugs and pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => {
   return (
     <Layout>
       <h1>Olá.</h1>
-      <h2>Meu nome é Gaeta, estudando Gatsby na bela Taubaté.</h2>
+      <h2>Meu nome é Gaeta, estudando Gatsby na calma Taubaté.</h2>
       <p>
         Precisa de um desenvolvedor?{" "}
         <Link to="/contact">Entre em contato.</Link>
